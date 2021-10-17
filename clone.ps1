cafb84baf568d8155cf92a60807b54b2937202c2
henryfbp <henryfbp@gmail.com>
2021-10-17 12:29:28 -0500

    initial commit

---
+++ clone.ps1
@@ -0,0 +1,36 @@
+$repoData = Import-Csv -Path .\projects.csv
+
+Push-Location repos
+
+foreach($_ in $repoData) {
+
+    # end of url
+    $foldername = ([System.Uri]$_.remote).Segments[-1]
+
+    # write-host "we want to clone $($_.remote) into $foldername"
+
+    # if path exists...
+    if(Test-Path -Path $foldername){
+        Write-Host "[+] Folder '$foldername' `t already exists, not cloning..."
+        continue;
+    }
+
+    git.exe clone $_.remote
+
+    if($_.sha1){
+        Write-Host " and checkout to commit/reference $($_.sha1)"
+        
+        # cd to the folder it was cloned to
+        Push-Location $foldername
+
+        git.exe checkout $_.sha1
+
+        Pop-Location
+
+        git checkout $_.sha1
+    } else {
+        Write-Host ""
+    }
+}
+
+Pop-Location
